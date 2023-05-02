659eef650239202d081b451357e2bb759f59b3f3
HieuPham2000 <hieu.pt183535@gmail.com>
2023-05-02 16:19:23 +0700

    feat; thêm constant error code

--- HUST.Core/Constants/ErrorCode.cs
+++ HUST.Core/Constants/ErrorCode.cs
@@ -11,60 +11,118 @@ namespace HUST.Core.Constants
     /// </summary>
     public static class ErrorCode
     {
-        public class LoginErrorCode
-        {
-            public const int WrongLoginInfo = 1001;
-            public const int UnactivatedEmail = 1002;
-        }
-
+        /// <summary>
+        /// Incorrect email or password
+        /// </summary>
         public const int Err1000 = 1000;
+
+        /// <summary>
+        /// Email already in use
+        /// </summary>
+        public const int Err1001 = 1001;
+
+        /// <summary>
+        /// Invalid account
+        /// </summary>
+        public const int Err1002 = 1002;
+
+        /// <summary>
+        /// Invalid verification token
+        /// </summary>
+        public const int Err1003 = 1003;
+
+        /// <summary>
+        /// Unactivated account
+        /// </summary>
         public const int Err1004 = 1004;
 
+        /// <summary>
+        /// Dictionary name already in use
+        /// </summary>
+        public const int Err2001 = 2001;
 
         /// <summary>
-        /// Mã lỗi khi xảy ra exception
+        /// Dictionary is in use
         /// </summary>
-        public const string ErrorCodeException = "ERR_001";
+        public const int Err2002 = 2002;
 
         /// <summary>
-        /// Mã lỗi khi validate trường bắt buộc thất bại
+        /// Source dictionary is empty
         /// </summary>
-        public const string ErrorCodeValidateRequired = "ERR_002";
+        public const int Err2003 = 2003;
 
         /// <summary>
-        /// Mã lỗi khi validate trường duy nhất thất bại
+        /// Concept already exists
         /// </summary>
-        public const string ErrorCodeValidateUnique = "ERR_003";
+        public const int Err3001 = 3001;
 
         /// <summary>
-        /// Mã lỗi khi validate định dạng thất bại
+        /// Concept can't be deleted
         /// </summary>
-        public const string ErrorCodeValidateFormat = "ERR_004";
+        public const int Err3002 = 3002;
 
         /// <summary>
-        /// Mã lỗi khi validate độ dài tối đa thất bại
+        /// A concept can't link to itself
         /// </summary>
-        public const string ErrorCodeValidateMaxLength = "ERR_005";
+        public const int Err3003 = 3003;
 
         /// <summary>
-        /// Mã lỗi khi request không hợp lệ
+        /// Circular link
         /// </summary>
-        public const string ErrorCodeBadRequest = "ERR_006";
+        public const int Err3004 = 3004;
 
         /// <summary>
-        /// Mã lỗi khi param gửi theo request không hợp lệ
+        /// Concept doesn't exist
         /// </summary>
-        public const string ErrorCodeRequestParam = "ERR_007";
+        public const int Err3005 = 3005;
 
         /// <summary>
-        /// Mã lỗi khi validate nghiệp vụ riêng thất bại
+        /// Duplicate examples
         /// </summary>
-        public const string ErrorCodeValidateCustom = "ERR_008";
+        public const int Err4001 = 4001;
+
+        /// <summary>
+        /// Invalid parameters
+        /// </summary>
+        public const int Err9000 = 9000;
+
+        /// <summary>
+        /// Invalid file upload
+        /// </summary>
+        public const int Err9001 = 9001;
+
+        /// <summary>
+        /// General error
+        /// </summary>
+        public const int Err9999 = 9999;
     }
 
-    public class ErrorMessage
+    /// <summary>
+    /// Thông báo lỗi
+    /// </summary>
+    public static class ErrorMessage
     {
         public const string Err1000 = "Incorrect email or password";
+        public const string Err1001 = "Email already in use";
+        public const string Err1002 = "Invalid account";
+        public const string Err1003 = "Invalid verification token";
         public const string Err1004 = "Unactivated account";
+
+        public const string Err2001 = "Dictionary name already in use";
+        public const string Err2002 = "Dictionary is in use";
+        public const string Err2003 = "Source dictionary is empty";
+
+        public const string Err3001 = "Concept already exists";
+        public const string Err3002 = "Concept can't be deleted";
+        public const string Err3003 = "A concept can't link to itself";
+        public const string Err3004 = "Circular link";
+        public const string Err3005 = "Concept doesn't exist";
+
+        public const string Err4001 = "Duplicate examples";
+
+        public const string Err9000 = "Invalid parameters";
+        public const string Err9001 = "Invalid file upload";
+
+
     }
 }
